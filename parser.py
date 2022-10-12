4dc70da566fbfc73035f86b9cbbcccf2c101e785
Sergey <2805026@gmail.com>
2022-10-12 17:07:38 +0300

    add ThreadPoolExecutor

--- parser.py
+++ parser.py
@@ -1,36 +1,52 @@
+from concurrent import futures
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 import config
+import time
+import datetime as dt
+from concurrent.futures import ThreadPoolExecutor, wait
 
 
-def add_to_log(log):
-    with open(config.PATH_LOG, "a") as f:
+def add_to_log(log, file_path):
+    with open(file_path, "a") as f:
         f.write(log)
 
 
 
-def main():
+def run_process(start_page, end_page, file_path, driver_count):
     wait_time = 2
-    start_page = 100086
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     driver = webdriver.Chrome(options=chrome_options)
-    driver.implicitly_wait(wait_time)
-    for i in range(start_page, start_page + 7):
+    for i in range(start_page, end_page):
+        file_path = f"{config.PATH_LOG}log_{start_page}-{end_page}.csv"
         driver.get(config.URL + str(i))
         try:
             WebDriverWait(driver, wait_time).until(
                 lambda driver: driver.find_element(By.CLASS_NAME, "CenterTitle")
                 )
-            print(str(i) + " - " + driver.find_element(By.CLASS_NAME, "CenterTitle").text)
-            add_to_log(config.URL + str(i) + " - " + driver.find_element(By.CLASS_NAME, "CenterTitle").text + '\n')
+            print(f"""D{driver_count} : {str(i)} - {driver.find_element(By.CLASS_NAME, "CenterTitle").text}""")
+            #add_to_log(f"""D{driver_count};{str(i)};{config.URL + str(i)};{driver.find_element(By.CLASS_NAME, "CenterTitle").text}\n""", file_path)
         except:
-            print(str(i) + " -  None")
-            # add_to_log(config.URL + str(i) + " -  None"+"\n")
+            print(f"D{driver_count} : {str(i)} - Empty")
+            #add_to_log(f"""D{driver_count};{str(i)};{config.URL + str(i)};Empty\n""", file_path)
     driver.quit()
+
+def main():
+    start_time = dt.datetime.now()
+    begin_page = int(input("Введите стартовую страницу: "))
+    page_step = 10
+    step_numbers = int(input(f"Введите количество шагов по {page_step} страниц: "))
+    stop_page = begin_page + page_step*step_numbers
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(run_process, i, i + page_step, f"{config.PATH_LOG}log_{i}-{i + page_step}.csv", (i - begin_page)//page_step) for i in range(begin_page, stop_page, page_step)
+                     for i in range(begin_page, stop_page, page_step)]
+        wait(futures)
+    end_time = dt.datetime.now()
+    print(f"Time taken: {end_time - start_time}")
 
 
 if __name__ == "__main__":
